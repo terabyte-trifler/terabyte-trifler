beaa5a6880cbf06ad9c326001185cc8bffb4775e
Gurnoor Singh <gurnoorgsingh@gmail.com>
2025-05-20 18:54:41 +0530

    using jwt

---
+++ class-2/index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "gurnoor123";
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+app.post("/signup", function(req,res){
+    const username = req.body.username
+    const password = req.body.password
+
+    users.push({
+        username : username,
+        password: password
+    })
+
+    // we should check if a user with this username already exists 
+
+    res.json({
+        message: "You are signed in"
+    })
+
+})
+
+app.post("/signin", function(req,res){
+    const username = req.body.username
+    const password = req.body.password
+
+    let foundUser = null;
+
+    for (let i=0; i< users.length;i++)
+    {
+        if(users[i].username === username && users[i].password === password){
+            foundUser = users[i]
+        }
+    }
+    
+    if(!foundUser){
+        res.json({
+            message: "Credentials incorrect"
+        })
+         return 
+    }
+    else
+    {
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+
+        res.json({
+            token: token
+        })
+    }
+})
+
+app.post("/me", function(req,res){
+
+    const token = req.headers.token;
+    const decodedData = jwt.verify(token, JWT_SECRET)
+
+    // const decodedData = jwt.decode(token)  Vulnerability 
+
+    if(decodedData.username) {
+       let foundUser = null;
+
+       for(let i=0; i<users.length; i++)
+       {
+        if(users[i].username === decodedData.username)
+        {
+            foundUser = users[i];
+        }
+       }
+
+       res.json({
+            username: foundUser.username,
+            password: foundUser.password
+       })
+    }
+
+    
+
+})
+
+
+app.listen(3031);
