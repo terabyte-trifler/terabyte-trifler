413438491be2074ee3997f898ba2a77bc7e0c50d
Gurnoor Singh <gurnoorgsingh@gmail.com>
2025-07-31 09:43:12 +0530

    useeffect dependency array cleanups

--- WebD/Assignments/week-9/9.2/reactPracticecounterapp/src/App.jsx
+++ WebD/Assignments/week-9/9.2/reactPracticecounterapp/src/App.jsx
@@ -4,11 +4,20 @@ import {useState, useEffect} from 'react';
 
 export default function App() {
   const[count, setCount] = useState(0);
+  const[count2, setCount2] = useState(0);
+
+  function increase() {
+    setCount(c => c +1)
+  }
+
+  function decrease() {
+    setCount2(c => c - 1)
+  }
 
   return <div>
     <Counter count ={count} />
     <button onClick={increase}>Increase Count</button>
-
+    <button onClick={decrease}>Decrease Count</button>
   </div>
 
 }
@@ -26,8 +35,17 @@ function Counter(props) {
     }
   }, []);
 
+  useEffect(function() {
+    console.log("count has changed");
+
+    return function() {
+      console.log("cleanup inside second effect");
+    }
+  }, [props.count])
+
   return <div>
-    Counter {props.count}
+    Counter1 {props.count} <br />
+    Counter2 {props.count2} <br />
   </div>
 
 }
