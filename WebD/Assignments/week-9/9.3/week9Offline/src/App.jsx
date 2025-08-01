d88b1f956d2ad7915b870e89d0f418a8321e7cb7
Gurnoor Singh <gurnoorgsingh@gmail.com>
2025-08-01 13:40:57 +0530

    HTml componeent

--- WebD/Assignments/week-9/9.3/week9Offline/src/App.jsx
+++ WebD/Assignments/week-9/9.3/week9Offline/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App () {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{background: "#dfe6e9", height: "100vh"}}>
+      <div style={{display: "flex", justifyContent: "center"}}>
+    <div>
+    <div><PostComponent /></div>
+    <div><PostComponent /></div>
+    <PostComponent />
+    </div>
+    </div>
+    </div>
   )
 }
+  // structuring your app into components
+  // Defining a state of your application
+const style= {width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1}
+  function PostComponent() {
+    return <div style={style}>
+    <div style={{display: "flex"}}>
+      <img src="https://images.unsplash.com/photo-1605559911160-a3d95d213904?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW9ua2V5fGVufDB8fDB8fHww" style={{
+        width: 30,
+        height: 30,
+        borderRadius: 20
+      }} />
+      <div style={{fontSize:10 ,marginLeft: 10}}>
+        <b>
+          100xdevs
+        </b>
+        <div>23,888 followers</div>
+        <div>12m</div>
+      </div>
 
-export default App
+      <div>
+        Want to know how to win big? Check out how these folks won $6000 in bounties
+      </div>
+    </div>
+    </div> 
+  }
