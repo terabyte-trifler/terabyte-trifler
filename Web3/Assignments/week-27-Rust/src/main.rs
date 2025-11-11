a00c9c5ea18f2c91efc95ec05409d68b7207bb85
Gurnoor Singh <gurnoorgsingh@gmail.com>
2025-11-11 18:55:31 +0530

    xyz

--- Web3/Assignments/week-27-Rust/src/main.rs
+++ Web3/Assignments/week-27-Rust/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    
+    let str = String::from("Harkirat");
+    let len = get_length(&str);
+    println!("{} {}", str, len);
+
 }
+
+fn get_length(str: &String) -> usize {
+    let len = str.len();
+    return len
+}
+
