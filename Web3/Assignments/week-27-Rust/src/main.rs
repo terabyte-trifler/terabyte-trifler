0eb80f327f820def4b10441919248a5c8ceb173f
Gurnoor Singh <gurnoorgsingh@gmail.com>
2025-11-26 10:07:32 +0530

    struct impl not passing &self as argument

--- Web3/Assignments/week-27-Rust/src/main.rs
+++ Web3/Assignments/week-27-Rust/src/main.rs
@@ -4,12 +4,11 @@ struct Rect{
 }
 
 impl Rect {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
+  fn print_str(){
+    println!("Inside the rect structure");
+  }
 }
 
 fn main() {
-    let rect = Rect { width: 30, height: 50 };
-    println!("{}", rect.area());  // Called ON the instance
+    Rect::print_str();
 }
